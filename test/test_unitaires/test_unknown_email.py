6844432a5db3d3d72e7f165ccbf995d3ae6c1ed6
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2022-12-12 12:38:17 +0100

    update test_unknown_email

--- test/test_unitaires/test_unknown_email.py
+++ test/test_unitaires/test_unknown_email.py
@@ -8,9 +8,7 @@ class TestLoginUnknownEmail:
     def test_valid_email(self):
         result = self.client.post("/showSummary",
                                   data={"email": server.clubs[0]["email"]})
-        data = result.data.decode()
         assert result.status_code == 200
-        assert f"{server.clubs[0]['email']}" in data
 
     def test_invalid_email(self):
         result = self.client.post("/showSummary",
