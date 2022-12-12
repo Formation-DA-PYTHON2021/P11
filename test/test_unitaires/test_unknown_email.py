e7e19985da856aff021cf3cf3f580000f82c936c
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2022-12-12 10:11:56 +0100

    debugok

--- test/test_unitaires/test_unknown_email.py
+++ test/test_unitaires/test_unknown_email.py
@@ -1,28 +1,18 @@
 from P11.server import app
 from P11 import server
 
+
 class TestLoginUnknownEmail:
     client = app.test_client()
 
-    club = [
-        {
-            "name": "Test_place_club",
-            "email": "test@test.com",
-            "points": "25"
-        }
-    ]
-
-    def setup_method(self):
-        server.clubs = self.club
-
     def test_valid_email(self):
         result = self.client.post("/showSummary",
-                                  data={"email": "test@test.com"})
+                                  data={"email": server.clubs[0]['email']})
+        print(server.clubs[0]['email'])
         assert result.status_code == 200
 
     def test_invalid_email(self):
         result = self.client.post("/showSummary",
                                   data={"email": "blabla@test.com"})
         assert result.status_code == 403
-
 
