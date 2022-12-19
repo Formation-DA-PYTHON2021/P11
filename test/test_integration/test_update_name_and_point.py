88e22e7812c958850f02ab200efb993e15ced2c0
Tiffany CALVET <tiffany.calvet@developpement-durable.gouv.fr>
2022-12-19 19:21:06 +0100

    test_integrate update name and points

---
+++ test/test_integration/test_update_name_and_point.py
@@ -0,0 +1,20 @@
+from P11 import server
+from P11.server import app
+
+
+class TestUpdateNamePoint:
+    client = app.test_client()
+
+    def test_udapte_name_points(self):
+        login = self.client.post(
+            "/showSummary",
+            data={"email": server.clubs[2]["email"]}
+        )
+        data = login.data.decode()
+
+        assert login.status_code == 200
+        assert f"<h2>Welcome, {server.clubs[2]['email']} </h2>" in data
+        assert f"<h4>Points available: {server.clubs[2]['points']}</h4>" in data
+
+
+
